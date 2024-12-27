7439c268a7d607cbf72899d2982dd207b0960359
whoelse <whoelse@git.com>
2024-12-28 01:18:42 +0530

    added requirements.txt

--- djangoMoneyTest/views.py
+++ djangoMoneyTest/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .forms import ReservationForm
-
+from djmoney.settings import CURRENCY_CHOICES
 
 
 def index(request):
 	form = ReservationForm()
+	print(CURRENCY_CHOICES)
 	return render(request, 'djangoMoneyTest/index.html', {'form': form})
